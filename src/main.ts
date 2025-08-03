8c3d89abcffeca6c7a3d726d0d7ad82a485ed925
DRAP-3312 <davidake117@gmail.com>
2025-08-03 11:33:33 -0500

    add lib  phosphor-icons

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import PhosphorIcons from '@phosphor-icons/vue'
 
 import App from './App.vue'
 import router from './router'
@@ -9,5 +10,6 @@ const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+app.use(PhosphorIcons)
 
 app.mount('#app')
