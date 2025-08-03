338fca2cc1b5e6706b19e905525c6a0c132b88b9
DRAP-3312 <davidake117@gmail.com>
2025-08-02 19:28:38 -0500

    install tailwind css 4 dependency

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,12 @@
 <script setup lang="ts"></script>
 
 <template>
-  <h1>Bienvenido a API Forge</h1>
-  <p>ApiForge proximamente...</p>
+  <div class="min-h-screen bg-gray-100 flex items-center justify-center">
+    <div class="text-center">
+      <h1 class="text-4xl font-bold text-blue-800 mb-4">Bienvenido a API Forge</h1>
+      <p class="text-lg text-gray-700">ApiForge proximamente...</p>
+    </div>
+  </div>
 </template>
 
 <style scoped></style>
